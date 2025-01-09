5f8124e73193ce25b38b957244a9658d70c05dac
PanayiotisEvangelou233096 <233096@buas.nl>
2025-01-09 16:02:31 +0100

    Update training_script.py

--- Y2B-2023-OT2_Twin/training_script.py
+++ Y2B-2023-OT2_Twin/training_script.py
@@ -1,29 +1,37 @@
+# Train the model locally
+import os
 from stable_baselines3 import PPO
 import gym 
 import time
 from clearml import Task
+from test_wrapper import OT2Env
 
-# Initialize ClearML task
 task = Task.init(project_name="Pendulum-v1/Panagiotis", 
                  task_name="Experiment V1.0")
 
-# Set the base Docker image
+# Add shimmy as a requirement
+task.set_requirements('shimmy'>=2.0)
+os.system("pip install 'shimmy>=2.0'")
+
 task.set_base_docker('deanis/2023y2b-rl:latest')
 
-# Execute the task remotely
 task.execute_remotely(queue_name="default")
 
-# Log the environment creation
-print("Creating the environment...")
-env = gym.make('Pendulum-v1', g=9.81)
-print("Environment created.")
+env = OT2Env(render=False, max_steps=1000)
 
-# Log the model initialization
-print("Initializing the model...")
-model = PPO("MlpPolicy", env, verbose=1)
-print("Model initialized.")
+hyperparameters = {
+    'n_steps': 2048,
+    'batch_size': 64,
+    'n_epochs': 10,
+    'gamma': 0.99,
+    'learning_rate': 3e-4,
+    'clip_range': 0.2,
+    'gae_lambda': 0.95,
+    'ent_coef': 0.01,
+    'vf_coef': 0.5,
+    'max_grad_norm': 0.5,
+}
 
-# Log the start of training
-print("Starting training...")
+model = PPO("MlpPolicy", env, verbose=1, **hyperparameters)
+
 model.learn(total_timesteps=10000, progress_bar=True)
-print("Training completed.")
